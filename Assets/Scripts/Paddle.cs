22a36f7844e0e0c759c761984837c0016ced540e
Jakub <49901045+gorsett@users.noreply.github.com>
2020-08-09 21:12:26 +0200

    Paddle can move now
    Controlled by mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //configuration parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 0f;
+    [SerializeField] float maxX = 16f;
+    //float mousePosX = Input.mousePosition.x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
