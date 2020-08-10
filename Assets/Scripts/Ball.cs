1d2a760900d0399dac0252945e0e58bad19c7480
Jakub <49901045+gorsett@users.noreply.github.com>
2020-08-10 22:03:08 +0200

    Launching mechanics
    + ball is sticked to the paddle
    + ball launches on mouse click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
+    bool hasStarted = false;
+
+    //state
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasStarted != true) //or !hasStarted
+        {
+            LockBallToPaddle();
+            LaunchOnClick();
+        }
+
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
