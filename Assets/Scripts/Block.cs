6ffbebcf7e105277732b5c597a0bb5798119cdf1
Jakub <49901045+gorsett@users.noreply.github.com>
2020-08-17 22:02:02 +0200

    Sound
    - sound when block is destroyed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] AudioClip blockBreakSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(blockBreakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
